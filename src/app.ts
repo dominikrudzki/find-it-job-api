8eba65217b78be5454afc89b0e50b89f4ce0a2d6
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-20 23:31:07 +0100

    add register route to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 require('dotenv').config({path: __dirname + '/config/.env'})
 import express from 'express'
+
 const app = express()
 app.use(express.json())
 
-// Middlewares
-const authRoute = require('./api/v1/routes/auth')
+const authRoute = require('./api/v1/routes/auth.route')
 app.use('/api/v1', authRoute)
+const registerRoute = require('./api/v1/routes/register.route')
+app.use('/api', registerRoute)
 
-app.listen(process.env.PORT,()=> {
+app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`)
 })
