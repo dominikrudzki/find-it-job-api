88939c3e5f193d5773de6d4d7431231f01f19ccd
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-23 22:01:54 +0100

    add cors and routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 require('dotenv').config({path: __dirname + '/config/.env'})
 import express from 'express'
 
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 const authRoute = require('./api/v1/routes/auth.route')
-app.use('/api/v1', authRoute)
-const registerRoute = require('./api/v1/routes/register.route')
-app.use('/api', registerRoute)
+app.use('/api', authRoute)
+const getJob = require('./api/v1/routes/get-job.route')
+app.use('/api', getJob)
+const getJobs = require('./api/v1/routes/get-jobs.route')
+app.use('/api', getJobs)
+
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`)
