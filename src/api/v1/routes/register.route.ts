e6a27a92541c30c2fe7ebfdce436ba425b196662
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-26 15:06:57 +0100

    add register validation

--- src/api/v1/routes/register.route.ts
+++ src/api/v1/routes/register.route.ts
@@ -1,47 +1,49 @@
-import bcrypt from 'bcrypt'
 import express from 'express'
+import bcrypt from 'bcrypt'
 import {pool} from "../../../db"
+import {body, validationResult} from 'express-validator';
 
 const router = express.Router()
 
-router.post('/v1/register', async (req: express.Request, res: express.Response) => {
+router.post(
+    '/v1/register',
+    body('email').isEmail(),
+    body('password').isLength({min: 5}),
+    body('employer').isBoolean(),
+    async (req: express.Request,
+           res: express.Response) => {
 
-    try {
-        const {account_type, email, password} = req.body
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
 
-        if (
-            !((account_type === 'employee' || account_type === 'employer') &&
-                email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) &&
-                password.length >= 5)
-        ) {
-            return res.status(400).json({error: 'Server error'})
-        }
+        try {
+            const {employer, email, password} = req.body
 
-        const db_user = await pool.query('SELECT * FROM "user" WHERE email = $1', [email.toLowerCase()])
+            const db_user = await pool.query('SELECT * FROM "user" WHERE email = $1', [email.toLowerCase()])
 
-        if (db_user.rows.length !== 0) {
-            return res.status(409).json({error: 'User with this email already exists'})
-        }
-
-        bcrypt.hash(password, 10, async (err: any, hash: any) => {
-            if (err) {
-                return res.status(err).json({error: 'Server error'})
+            if (db_user.rows.length !== 0) {
+                return res.status(409).json({error: 'User with this email already exists'})
             }
-            await pool.query(
-                'INSERT INTO "user" (email, password, employee, employer) VALUES ($1, $2, $3, $4)',
-                [email.toLowerCase(), hash, account_type === 'employee', account_type === 'employer'],
-                (err: any) => {
-                    if (err) {
-                        return res.status(500).json({error: 'Unable to create account'})
-                    } else {
-                        return res.status(200).json({status: 'Success'})
-                    }
+
+            bcrypt.hash(password, 10, async (err: any, hash: any) => {
+                if (err) {
+                    return res.status(err).json({error: 'Server error'})
                 }
-            )
-        })
-    } catch (err: any) {
-        res.status(500).json({error: err.message})
-    }
-})
+                await pool.query(
+                    'INSERT INTO "user" (email, password, employer) VALUES ($1, $2, $3)',
+                    [email.toLowerCase(), hash, employer],
+                    (err: any) => {
+                        if (err) {
+                            return res.status(500).json({error: 'Unable to create account'})
+                        } else {
+                            return res.status(200).json({status: 'Success'})
+                        }
+                    }
+                )
+            })
+        } catch (err: any) {
+            res.status(500).json({error: err.message})
+        }
+    })
 
 export {router as register}
