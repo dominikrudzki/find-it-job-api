b64250315258b914dc401c976cb0fb11389053a5
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-27 23:54:21 +0100

    create login.controller.ts

---
+++ src/api/v1/controllers/login.controller.ts
@@ -0,0 +1,54 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import { pool } from "../../../config/db"
+import { validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+
+export const login = async (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()})
+
+    try {
+        const {email, password} = req.body
+
+        const db_user = await pool.query(
+            'SELECT * FROM "user" WHERE email = $1',
+            [email.toLowerCase()]
+        )
+
+        if (db_user.rows.length === 0) {
+            return res.status(404).json({error: 'User does not exist'})
+        }
+
+        bcrypt.compare(password, db_user.rows[0].password, async (err: Error | undefined, result: boolean) => {
+            if (err) {
+                return res.status(500).json({error: 'Server error'})
+            } else if (result) {
+
+                // LOGIN SUCCESS
+
+                const accessToken = jwt.sign(
+                    {email: email.toLowerCase(), employer: db_user.rows[0].employer},
+                    process.env.ACCESS_TOKEN_SECRET + db_user.rows[0].password,
+                    {expiresIn: '30m'}
+                )
+
+                const refreshToken = jwt.sign(
+                    {email: email.toLowerCase()},
+                    process.env.REFRESH_TOKEN_SECRET + db_user.rows[0].password,
+                    {expiresIn: '200d'}
+                )
+
+                return res.status(200).json({accessToken, refreshToken})
+            } else {
+                return res.status(404).json({error: 'Invalid password'})
+            }
+        })
+    } catch (err) {
+        res.status(500).json({error: 'Server error'})
+    }
+}
+
+export const loginTest = (req: express.Request, res: express.Response) => {
+    res.status(200).json({status: 'You are logged in'})
+}
