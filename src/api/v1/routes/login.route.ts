52d542c367cce0fa1f53966b3f673a3a70626bd3
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-27 20:25:20 +0100

    update login route logic

--- src/api/v1/routes/login.route.ts
+++ src/api/v1/routes/login.route.ts
@@ -22,30 +22,32 @@ router.post(
         try {
             const {email, password} = req.body
 
-            const db_user = await pool.query('SELECT * FROM "user" WHERE email = $1', [email.toLowerCase()])
+            const db_user = await pool.query(
+                'SELECT * FROM "user" WHERE email = $1',
+                [email.toLowerCase()]
+            )
 
-            if (db_user.rows.length === 0) return res.status(404).json({error: 'User does not exist'})
+            if (db_user.rows.length === 0) {
+                return res.status(404).json({error: 'User does not exist'})
+            }
 
-            bcrypt.compare(password, db_user.rows[0].password, async (err: any, result: any) => {
+            bcrypt.compare(password, db_user.rows[0].password, async (err: Error | undefined, result: boolean) => {
                 if (err) {
-                    return res.status(err).json({error: 'Server error'})
+                    return res.status(500).json({error: 'Server error'})
                 } else if (result) {
+
                     // LOGIN SUCCESS
+
                     const accessToken = jwt.sign(
-                        {email, employer: db_user.rows[0].employer},
-                        process.env.ACCESS_TOKEN_SECRET!,
+                        {email: email.toLowerCase(), employer: db_user.rows[0].employer},
+                        process.env.ACCESS_TOKEN_SECRET + db_user.rows[0].password,
                         {expiresIn: '30m'}
                     )
 
                     const refreshToken = jwt.sign(
-                        {},
-                        process.env.REFRESH_TOKEN_SECRET!,
-                        {expiresIn: '90d'}
-                    )
-
-                    await pool.query(
-                        'UPDATE "user" SET refresh_token = $1 WHERE "user".id = $2',
-                        [refreshToken, db_user.rows[0].id]
+                        {email: email.toLowerCase()},
+                        process.env.REFRESH_TOKEN_SECRET + db_user.rows[0].password,
+                        {expiresIn: '200d'}
                     )
 
                     return res.status(200).json({accessToken, refreshToken})
@@ -53,9 +55,8 @@ router.post(
                     return res.status(404).json({error: 'Invalid password'})
                 }
             })
-        } catch (err: any) {
-            console.log('erro')
-            res.status(500).json({error: err.message})
+        } catch (err) {
+            res.status(500).json({error: 'Server error'})
         }
     })
 
