bc1f829a1c103fe271b354a44e6620928a4a1d40
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-28 00:02:41 +0100

    update imports in app

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-
-import {getJob} from './api/v1/routes/get-job.route'
-import {getJobs} from './api/v1/routes/get-jobs.route'
-import {register} from './api/v1/routes/register.route'
-import {login} from './api/v1/routes/login.route'
-import {refreshToken} from './api/v1/routes/refresh-token.route'
+import { validatePath } from "./middlewares/validatePath"
+import job from "./api/v1/routes/job.route"
+import auth from "./api/v1/routes/auth.route"
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
-dotenv.config()
+dotenv.config({path: __dirname + '/config/.env'})
 
-app.use('/api', getJob)
-app.use('/api', getJobs)
-app.use('/api', register)
-app.use('/api', login)
-app.use('/api', refreshToken)
+app.use('/api/v1', job)
+app.use('/api/v1', auth)
+app.use('/company_logos', express.static('src/public/resources/company_logos'), validatePath)
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`)
