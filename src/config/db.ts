38fcf624456c17bf6581ad758cd96633e11f4dd1
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-27 23:52:12 +0100

    move config files to config directory

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,13 @@
-import {Pool} from 'pg'
+import { Pool } from 'pg'
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({path: __dirname + '/.env'})
 
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD
 })
 
-export {pool}
+export { pool }
