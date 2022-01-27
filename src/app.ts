c057744c42487a6737492451c5d94bb7efaead1b
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-27 20:24:17 +0100

    import refresh-route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import {getJob} from './api/v1/routes/get-job.route'
 import {getJobs} from './api/v1/routes/get-jobs.route'
 import {register} from './api/v1/routes/register.route'
 import {login} from './api/v1/routes/login.route'
+import {refreshToken} from './api/v1/routes/refresh-token.route'
 
 const app = express()
 app.use(cors())
@@ -16,6 +17,7 @@ app.use('/api', getJob)
 app.use('/api', getJobs)
 app.use('/api', register)
 app.use('/api', login)
+app.use('/api', refreshToken)
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`)
