9ba464f31a9ac68a45a4afe7fb9d5514482a58c7
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-27 23:53:31 +0100

    create get-jobs.controller.ts

--- src/api/v1/controllers/get-jobs.controller.ts
+++ src/api/v1/controllers/get-jobs.controller.ts
@@ -1,9 +1,7 @@
-import express from "express";
+import express from 'express'
+import { pool } from "../../../config/db"
 
-const pool = require("../../../db");
-const router = require('express').Router()
-
-router.get('/v1/get-jobs', async (req: express.Request, res: express.Response) => {
+export const getJobs = async (req: express.Request, res: express.Response) => {
     try {
         // TODO: order by date newest first and limit to e.g. 10
         const jobData = await pool.query(`SELECT j.id, j.name, e.company_image image, e.company_name company, j.remote, j.salary, j.experience, j.skills FROM job j INNER JOIN employer e ON j.employer_id = e.id`)
@@ -11,6 +9,4 @@ router.get('/v1/get-jobs', async (req: express.Request, res: express.Response) =
     } catch (err: any) {
         res.status(404).json({error: err.message})
     }
-})
-
-export {router as getJobs}
+}
