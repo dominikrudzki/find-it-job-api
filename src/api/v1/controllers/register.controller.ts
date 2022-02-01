ea7358dcc4be25d66eea4ef3ed4ec49cd0fa08ca
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-01 14:00:47 +0100

    update logic in register.controller.ts

--- src/api/v1/controllers/register.controller.ts
+++ src/api/v1/controllers/register.controller.ts
@@ -2,21 +2,30 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import { pool } from "../../../config/db"
 import { validationResult } from 'express-validator'
+import { QueryResult } from "pg"
+import { UserData } from "../../../models/UserData"
 
 export const register = async (req: express.Request, res: express.Response) => {
 
     const errors = validationResult(req)
-    if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()})
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
 
     try {
         const {employer, email, password} = req.body
 
-        const db_user = await pool.query(
+        if (employer && !(!!req.body.company_name && req.body.company_name.length >= 3)) {
+            return res.status(406).json({error: 'Wrong company name'})
+        }
+
+        const userData: QueryResult<UserData> = await pool.query(
             'SELECT id FROM "user" WHERE email = $1',
             [email.toLowerCase()]
         )
 
-        if (db_user.rows.length !== 0) {
+        if (userData.rows.length !== 0) {
             return res.status(409).json({error: 'User with this email already exists'})
         }
 
@@ -25,13 +34,27 @@ export const register = async (req: express.Request, res: express.Response) => {
                 return res.status(500).json({error: 'Server error'})
             }
             await pool.query(
-                'INSERT INTO "user" (email, password, employer) VALUES ($1, $2, $3)',
+                'INSERT INTO "user" (email, password, employer) VALUES ($1, $2, $3) RETURNING id',
                 [email.toLowerCase(), hash, employer],
-                (err: any) => {
+                async (err, result) => {
                     if (err) {
-                        return res.status(500).json({error: 'Unable to create account'})
+                        return res.status(500).json({error: 'Unable to create account 1'})
                     } else {
-                        return res.status(200).json({status: 'Success'})
+                        if (employer) {
+                            await pool.query(
+                                'INSERT INTO employer (company_name, user_id) VALUES ($1, $2)',
+                                [req.body.company_name, result.rows[0].id],
+                                (err: Error) => {
+                                    if (err) {
+                                        return res.status(500).json({error: 'Unable to create account 2'})
+                                    } else {
+                                        return res.status(200).json({status: 'Success'})
+                                    }
+                                }
+                            )
+                        } else {
+                            return res.status(200).json({status: 'Success'})
+                        }
                     }
                 }
             )
