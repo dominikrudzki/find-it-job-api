8793a837ce8622bc51e8efb3592df5dfc09ed248
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-01 14:04:27 +0100

    add change password route

--- src/api/v1/routes/auth.ts
+++ src/api/v1/routes/auth.ts
@@ -4,6 +4,8 @@ import { register } from "../controllers/register.controller"
 import { login, loginTest } from "../controllers/login.controller"
 import { authenticateToken } from "../../../middlewares/authenticateToken"
 import { refreshToken } from "../controllers/refreshToken.controller"
+import { changePassword } from "../controllers/changePassword.controller"
+import { differentPassword } from "../../../middlewares/differentPassword"
 
 const router = express.Router()
 
@@ -23,14 +25,29 @@ router.post(
 )
 
 router.post(
-    '/v1/login-test',
+    '/login-test',
     authenticateToken,
     loginTest
 )
 
+router.patch(
+    '/change-password',
+    authenticateToken,
+    body('password').isLength({min: 5}),
+    body('newPassword').isLength({min: 5}),
+    differentPassword,
+    changePassword
+)
+
 router.post(
-    '/v1/refresh-token',
+    '/refresh-token',
     refreshToken
 )
+
+// router.patch(
+//     '/recover-password',
+//     authenticateToken,
+//     recoverPassword
+// )
 
 export default router
