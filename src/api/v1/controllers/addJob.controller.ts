0714a5f1b66091f6fa88b4b47ceafc738c5f3804
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:41:54 +0100

    delete requirements from query

--- src/api/v1/controllers/addJob.controller.ts
+++ src/api/v1/controllers/addJob.controller.ts
@@ -1,30 +1,27 @@
 import express from "express"
 import { pool } from "../../../config/db"
-import jwt from "jsonwebtoken"
 import { QueryResult } from "pg"
 import { Job } from "../../../models/Job"
+// @ts-ignore
 import { UserData } from "../../../models/UserData"
 
 export const addJob = async (req: express.Request, res: express.Response) => {
 
-    const bearer: string[] = req.headers.authorization!.split(' ')
-    const accessTokenPayload: any = jwt.decode(bearer[1], {complete: true})!.payload
-
     try {
         const {name, remote, salary, description, benefits, requirements, skills, experience}: Job = req.body
 
-        const userData: QueryResult<UserData> = await pool.query(
+        const userData: QueryResult<UserData> = await pool.query( // TODO: optimize (get id from token payload)
             'SELECT id FROM "user" WHERE email = $1',
-            [accessTokenPayload.email.toLowerCase()]
+            // @ts-ignore
+            [req.jwtPayload.email]
         )
 
         await pool.query(
-            'INSERT INTO job (name, remote, salary, description, benefits, requirements, skills, employer_id, experience) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-            [name, remote, salary, description, benefits, requirements, JSON.stringify(skills), userData.rows[0].id, experience]
+            'INSERT INTO job (name, remote, salary, description, benefits, skills, employer_id, experience) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+            [name, remote, salary, description, benefits, JSON.stringify(skills), userData.rows[0].id, experience]
         )
         res.status(200).json({status: 'ok'})
     } catch (err) {
-        console.log(err)
         res.status(500).json({error: 'Server error'})
     }
 }
