db045a44a6f4f88d4b4e3a4e06504c078d56a9ee
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:38:18 +0100

    update getJobs controller, display jobs from DESC

--- src/api/v1/controllers/getJobs.controller.ts
+++ src/api/v1/controllers/getJobs.controller.ts
@@ -3,8 +3,7 @@ import { pool } from "../../../config/db"
 
 export const getJobs = async (req: express.Request, res: express.Response) => {
     try {
-        // TODO: order by date newest first and limit to e.g. 10
-        const jobData = await pool.query(`SELECT j.id, j.name, e.company_image image, e.company_name company, j.remote, j.salary, j.experience, j.skills FROM job j INNER JOIN employer e ON j.employer_id = e.id`)
+        const jobData = await pool.query(`SELECT j.id, j.name, e.company_image image, e.company_name company, j.remote, j.salary, j.experience, j.skills FROM job j INNER JOIN employer e ON j.employer_id = e.id ORDER BY j.id DESC`)
         res.json(jobData.rows)
     } catch (err) {
         res.status(404).json({error: 'Server error'})
