a4634c10e9e8f580f556e5b21e05867b044cc38d
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:37:22 +0100

    create delete-job route

--- src/api/v1/routes/job.ts
+++ src/api/v1/routes/job.ts
@@ -4,6 +4,7 @@ import { getJob } from "../controllers/getJob.controller"
 import { authenticateToken } from "../../../middlewares/authenticateToken"
 import { isEmployer } from "../../../middlewares/isEmployer"
 import { addJob } from "../controllers/addJob.controller"
+import { deleteJob } from "../controllers/detete-job.controller"
 
 const router = express.Router()
 
@@ -22,6 +23,13 @@ router.put(
     authenticateToken,
     isEmployer,
     addJob
+)
+
+router.delete(
+    '/delete-job',
+    authenticateToken,
+    isEmployer,
+    deleteJob
 )
 
 export default router
