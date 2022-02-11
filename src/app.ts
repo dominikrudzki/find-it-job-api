33bee461563c877b93fd790c96e8dcc5af5c3dfb
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-11 22:37:29 +0100

    update imports

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
-import { validatePath } from "./middlewares/validatePath"
 import job from "./api/v1/routes/job"
 import auth from "./api/v1/routes/auth"
-import company from "./api/v1/routes/company"
+import employer from "./api/v1/routes/employer"
+import developer from "./api/v1/routes/developer"
+import { validatePath } from "./middlewares/validate-path"
 
 const app = express()
 
@@ -14,7 +15,8 @@ dotenv.config({path: __dirname + '/config/.env'})
 
 app.use('/api/v1', job)
 app.use('/api/v1', auth)
-app.use('/api/v1', company)
+app.use('/api/v1', employer)
+app.use('/api/v1', developer)
 app.use('/company_logos', express.static('src/public/resources/company_logos'), validatePath)
 
 app.listen(process.env.PORT, () => {
