bbfeda0059d87c76f36ad1dcdfcd739e97e5a44f
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-15 23:25:04 +0100

    update getJobs query & add params to route

--- src/api/v1/controllers/get-jobs.controller.ts
+++ src/api/v1/controllers/get-jobs.controller.ts
@@ -1,11 +1,43 @@
 import express from 'express'
 import { pool } from "../../../config/db"
+import { validationResult } from "express-validator"
 
 export const getJobs = async (req: express.Request, res: express.Response) => {
+
     try {
+        const {remote, experience, salary, skills} = req.body
+
+        const values = [parseInt(req.params.count)]
+
+        let whereCondition = 'j.id >= 0'
+
+        if (remote !== undefined) {
+            values.push(remote)
+            whereCondition += ` AND j.remote = $${values.length}`
+        }
+
+        if (experience !== undefined) {
+            values.push(experience)
+            whereCondition += ` AND j.experience = $${values.length}`
+        }
+
+        if (salary !== undefined) {
+            values.push(salary)
+            whereCondition += ` AND (j.salary ->> 'max')::DECIMAL > $${values.length}`
+        }
+
+        if (skills !== undefined) {
+            values.push(skills)
+            whereCondition += ` AND to_json(array(select jsonb_array_elements(j.skills) ->> 'name'))::jsonb ?| $${values.length}`
+        }
+
         const jobData = await pool.query(
-            `SELECT j.id, j.name, e.company_image image, e.company_name company, j.remote, j.salary, j.experience, j.skills FROM job j INNER JOIN employer e ON j.employer_id = e.id ORDER BY j.id DESC OFFSET $1 LIMIT 5`,
-            [parseInt(req.params.count)]
+            `SELECT j.id, j.name, e.company_image image, e.company_name company, j.remote, j.salary, j.experience, j.skills 
+            FROM job j 
+            INNER JOIN employer e ON j.employer_id = e.id
+            WHERE ${whereCondition}
+            ORDER BY j.id DESC OFFSET $1 LIMIT 5`,
+            values
         )
         res.json(jobData.rows)
     } catch (err) {
