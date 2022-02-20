b59ee86bbda4f7cb867b4c8dc28210c995860b09
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-20 14:48:35 +0100

    fix apply for job query

--- src/api/v1/controllers/apply-for-job.controller.ts
+++ src/api/v1/controllers/apply-for-job.controller.ts
@@ -4,7 +4,15 @@ import { pool } from "../../../config/db"
 export const applyForJob = async (req: express.Request, res: express.Response) => {
     try {
         await pool.query(
-            'INSERT INTO job_application (user_id , job_id) SELECT u.id, $1 FROM "user" u WHERE u.email = $2 AND NOT EXISTS (SELECT user_id, job_id FROM job_application WHERE job_id = $1 AND u.email = $2)',
+            `INSERT INTO job_application (user_id , job_id) 
+            SELECT u.id, $1 
+            FROM "user" u 
+            WHERE u.email = $2 
+            AND NOT EXISTS (
+                SELECT user_id, job_id 
+                FROM job_application 
+                WHERE user_id = u.id AND job_id = $1
+            )`,
             // @ts-ignore
             [parseInt(req.params.jobId), req.jwtPayload.email]
         )
