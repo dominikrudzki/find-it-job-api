782ab857f1ca684e04902e5b078edb13140c24db
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-22 00:06:14 +0100

    update UserData interface

--- src/models/user-data.ts
+++ src/models/user-data.ts
@@ -1,6 +1,8 @@
 export interface UserData {
-    id: number,
-    email: string,
-    password: string,
-    employer: boolean,
+    id: number
+    email: string
+    password: string
+    company_name?: string
+    company_image?: string
+    employer: boolean
 }
